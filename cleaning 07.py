3d75f28a76e184a656cc8ee4f4ae74c26f046098
bgm3167 <rhksals2001@gmail.com>
2023-10-27 11:53:31 +0900

    전처리 및 토큰화 과정 및 작업물(7월)

--- cleaning 07.py
+++ cleaning 07.py
@@ -7,37 +7,68 @@ df = pd.read_csv("C:/Users/rhksa/OneDrive/바탕 화면/gs project/GS_team/news_
 df2 = df[['title','date','content']]
 df2['content'] = df['content'].str.replace('\n',' ').replace('\t',' ').replace('\r', ' ')
 
-from konlpy.tag import Komoran
+import MeCab
+import pandas as pd
 import re
-tokenizer = Komoran()
 
-df2['morphs'] = None
-for i, row in df.iterrows():
-    df2['morphs'][i] = ' '.join(tokenizer.morphs(row['content']))
+# Mecab 인스턴스 생성
+m = MeCab.Tagger()
 
-# 한글 문자만 남기고 나머지 문자 제거
-df2['morphs'] = df2['morphs'].apply(lambda x: re.sub('[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]', '', x))
+# 'title' 열의 결과를 'titl' 열에 저장
+df2['titl'] = df2['title'].apply(lambda x: re.sub('[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]', ' ', x))
 
-import spacy
-
-# spaCy의 NLP 모델을 로드합니다.
-nlp = spacy.load('en_core_web_sm')
+# 'content' 열의 결과를 'cont' 열에 저장
+df2['cont'] = df2['content'].apply(lambda x: re.sub('[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]', ' ', x))
 
 def extract_words(text):
-    # 텍스트를 spaCy의 NLP 모델을 사용하여 토큰화합니다.
-    doc = nlp(text)
-      
-    # 길이가 2글자 이상이고, 명사(Noun), 동사(Verb), 형용사(Adjective)인 단어를 선택합니다.
-    selected_words = [token.text for token in doc if len(token.text) >= 2 and token.pos_ in ['NOUN', 'VERB', 'ADJ']]
-      
-    # 선택된 단어를 공백으로 구분된 문자열로 결합합니다.
+    # 텍스트를 Mecab을 사용하여 토큰화합니다.
+    tokens = m.parse(text).split('\n')
+
+    # 선택된 단어를 저장할 리스트
+    selected_words = []
+
+    for token in tokens:
+        if len(token) >= 2:
+            # 각 토큰을 탭(\t)으로 분리하여 형태소와 품사 정보를 얻습니다.
+            parts = token.split('\t')
+            if len(parts) == 2:
+                word, pos = parts
+                pos = pos.split(',')[0]  # 품사 정보에서 첫 번째 부분만 사용
+
+                # 선택 조건 (명사, 동사, 형용사)을 확인하고 선택된 단어 리스트에 추가
+                if len(word) >= 2 and pos in ['NNG', 'VV', 'VA']:
+                    selected_words.append(word)
+
+    # 선택된 단어를 공백으로 구분된 문자열로 반환합니다.
     return ' '.join(selected_words)
 
-df2['morphs'] = df2['morphs'].apply(extract_words)
+# 'title' 및 'content' 열에 대해 처리
+for i, row in df2.iterrows():
+    df2.at[i, 'titl'] = extract_words(row['title'])
+    df2.at[i, 'cont'] = extract_words(row['content'])
 
-df3 = df2[['title','date','morphs']]
+df3 = df2[['date','titl','cont']]
 
-stop_word = stop_word = ['에서','ㄴ다','ㅂ니다','있다','으로','밝히','이번','다는','습니다','어서','ㄴ데','역시','이어','추가','관련','최근','오후','많이']
+from collections import Counter
+
+words_list = df3['cont'].str.split()
+
+all_words = [word for words in words_list for word in words]
+
+word_counts = Counter(all_words)
+
+most_common_words = word_counts.most_common(50)
+print("가장 많이 나온 상위 50개 단어:")
+for word, count in most_common_words:
+    print(f"{word}: {count}")
+
+
+stop_word = ['기자','지난해','올해']
+
+def preprocess(text):
+  text = text.split()
+  text = [i for i in text if i not in stop_word]
+  return text
 
 def preprocess(text):
   text = text.split()
@@ -49,23 +80,29 @@ def make_tokens(df):
   for i, row in df.iterrows():
     if i%100==0:
       print(i,'/',len(df))
-    token = preprocess(df['morphs'][i])
+    token = preprocess(df['cont'][i])
     df['tokens'][i] = ' '.join(token)
   return df
 
 df4 = make_tokens(df3)
+
+# 이후 불용어 확인 후 반복 이때는 불용어 처리시 이 코드 사용
+#from collections import Counter
+
+#words_list = df4['tokens'].str.split()
+
+#all_words = [word for words in words_list for word in words]
+
+#word_counts = Counter(all_words)
+
+#most_common_words = word_counts.most_common(50)
+#print("가장 많이 나온 상위 50개 단어:")
+#for word, count in most_common_words:
+    #print(f"{word}: {count}")
+
+#날짜만 추출
+df4['date'] = df4['date'].str.split(' ').str[0]
 print(df4)
 
-#불용어처리 확인
-from collections import Counter
-
-words_list = df4['tokens'].str.split()
-
-all_words = [word for words in words_list for word in words]
-
-word_counts = Counter(all_words)
-
-most_common_words = word_counts.most_common(50)
-print("가장 많이 나온 상위 50개 단어:")
-for word, count in most_common_words:
-    print(f"{word}: {count}")
+#csv파일 만들기
+df4.to_csv('파일명.csv', index=False)
