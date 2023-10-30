623ce2e676d3888ea5c3c23e3cb9ed46810616d2
LeeJiwoo982 <easywoo980902@gmail.com>
2023-10-30 11:16:47 +0900

    2207 크롤링,전처리완료

--- 01pretreatment/code/news/news_pretreat(ver.jw).py
+++ 01pretreatment/code/news/news_pretreat(ver.jw).py
@@ -10,7 +10,7 @@ from collections import Counter
 warnings.filterwarnings(action = 'ignore')
 
 # csv파일 불러오기
-df = pd.read_csv("C:\\DA\\GSTEAM\\GS_team\\01pretreatment\\csv\\news\\raw_data\\R_gs_news2211.csv")
+df = pd.read_csv("C:\\DA\\GSTEAM\\GS_team\\01pretreatment\\csv\\news\\raw_data\\R_gs_news2207.csv")
 
 # df2에 필요정보만 저장
 df2 = df[['date','title','content']]
@@ -62,16 +62,16 @@ df3 = df2[['date','Title','Content']]
 # 제목은 처리 안함
 # 단어 빈도수 오름차순 정리
 # 불용어 리스트 작성
-Cwords_list = df3['Content'].str.split()
-all_Cwords = [word for words in Cwords_list for word in words]
-Cword_counts = Counter(all_Cwords)
-most_common_Cwords = Cword_counts.most_common(500)
+# Cwords_list = df3['Content'].str.split()
+# all_Cwords = [word for words in Cwords_list for word in words]
+# Cword_counts = Counter(all_Cwords)
+# most_common_Cwords = Cword_counts.most_common(500)
 
-print("제목에서 가장 많이 나온 상위 50개 단어:")
-for word, count in most_common_Cwords:
-    print(f"{word}: {count}")
+# print("제목에서 가장 많이 나온 상위 50개 단어:")
+# for word, count in most_common_Cwords:
+#     print(f"{word}: {count}")
 
-stop_Cword = ['건설','건설사','올해','최근','이번','상황','가운데','활용','이후','지난해','관련']
+stop_Cword = ['건설','건설사','올해','최근','이번','상황','가운데','활용','이후','지난해','관련','따르','기간','해당','기자']
 
 def preprocess(text):
   text = text.split()
@@ -89,4 +89,4 @@ def make_tokens(df):
 
 DF = make_tokens(df3)
 print(DF)
-DF.to_csv('gs_news2212(ver.C01).csv', index = False)
+DF.to_csv('gs_news2207(ver.C01).csv', index = False)
