88d88c7f92862e0af21340cea633035866394134
LeeJiwoo982 <easywoo980902@gmail.com>
2023-11-01 10:45:10 +0900

    관민님 lda코드 다시한번 확인

---
+++ 02LDA/lda(ver.jw1.1).py
@@ -0,0 +1,27 @@
+import warnings
+warnings.filterwarnings(action = 'ignore')
+import pandas as pd
+from gensim import corpora
+from gensim.models import LdaModel, TfidfModel
+import pyLDAvis
+import pyLDAvis.gensim
+
+df = pd.read_csv('C:\\DA\\GSTEAM\\GS_team\\01pretreatment\\stock_news_combine.csv')
+
+tokenized_docs = df['Tokens_2'].apply(lambda x: x.split())
+id2word = corpora.Dictionary(tokenized_docs)
+corpus_TDM =[id2word.doc2bow(doc) for doc in tokenized_docs]
+tfidf =  TfidfModel(corpus_TDM)
+corpus_TFIDF = tfidf[corpus_TDM]
+n = 15
+lda = LdaModel(corpus=corpus_TFIDF,
+               id2word=id2word,
+               num_topics=n,
+               random_state=100)
+
+for t in lda.print_topics() :
+  print(t)
+
+pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(lda, corpus_TFIDF, id2word)
+pyLDAvis.display(vis)
