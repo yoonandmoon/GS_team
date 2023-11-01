44cf6144e79a7d18404369397c7914fe3651352f
LeeJiwoo982 <easywoo980902@gmail.com>
2023-11-01 17:56:34 +0900

    다같이 쓰는 전처리 코드

--- 01pretreatment/code/news/news_pretreat(ver.jw).py
+++ 01pretreatment/code/news/news_pretreat(ver.jw).py
@@ -62,31 +62,32 @@ df3 = df2[['date','Title','Content']]
 # 제목은 처리 안함
 # 단어 빈도수 오름차순 정리
 # 불용어 리스트 작성
-# Cwords_list = df3['Content'].str.split()
-# all_Cwords = [word for words in Cwords_list for word in words]
-# Cword_counts = Counter(all_Cwords)
-# most_common_Cwords = Cword_counts.most_common(500)
+Cwords_list = df3['Content'].str.split()
+all_Cwords = [word for words in Cwords_list for word in words]
+Cword_counts = Counter(all_Cwords)
+most_common_Cwords = Cword_counts.most_common(500)
 
-# print("제목에서 가장 많이 나온 상위 500개 단어:")
-# for word, count in most_common_Cwords:
-#     print(f"{word}: {count}")
+print("제목에서 가장 많이 나온 상위 500개 단어:")
+for word, count in most_common_Cwords:
+    print(f"{word}: {count}")
 
-stop_Cword = ['건설','건설사','올해','최근','이번','상황','가운데','활용','이후','지난해','관련','따르','기간','해당','기자']
+# 불용어 사전 만들고 나서 진행하기
+# stop_Cword = ['건설','건설사','올해','최근','이번','상황','가운데','활용','이후','지난해','관련','따르','기간','해당','기자']
 
-def preprocess(text):
-  text = text.split()
-  text = [i for i in text if i not in stop_Cword]
-  return text
+# def preprocess(text):
+#   text = text.split()
+#   text = [i for i in text if i not in stop_Cword]
+#   return text
 
-def make_tokens(df):
-  df['tokens'] = ' '
-  for i, row in df.iterrows():
-    if i%100==0:
-      print(i,'/',len(df))
-    token = preprocess(df['Content'][i])
-    df['tokens'][i] = ' '.join(token)
-  return df
+# def make_tokens(df):
+#   df['tokens'] = ' '
+#   for i, row in df.iterrows():
+#     if i%100==0:
+#       print(i,'/',len(df))
+#     token = preprocess(df['Content'][i])
+#     df['tokens'][i] = ' '.join(token)
+#   return df
 
-DF = make_tokens(df3)
-print(DF)
-DF.to_csv('gs_news2310(ver.C01).csv', index = False)
+# DF = make_tokens(df3)
+# print(DF)
+# DF.to_csv('gs_news2310(ver.C01).csv', index = False)
